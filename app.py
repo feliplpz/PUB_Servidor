48ed3bdf0315e5a15c92b7c98a1df9e96f911c85
Felipe Lopez <felipe_lopez@usp.br>
2025-07-02 00:49:33 -0300

    formata documento

--- app.py
+++ app.py
@@ -1,15 +1,16 @@
-import logging
 import asyncio
+import logging
+
 import uvicorn
+from dotenv import load_dotenv
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
-from dotenv import load_dotenv
+
 from src.connection.bluetooth_server import BluetoothConnection
+from src.connection.websocket_manager import WebSocketManager
 from src.utils.logging import Logger
 from src.web.routes import register_routes
-from src.connection.websocket_manager import WebSocketManager
-
 
 # Carrega variáveis de ambiente
 load_dotenv()
@@ -24,19 +25,20 @@ app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 
-register_routes(app,templates,websocket_manager)
+register_routes(app, templates, websocket_manager)
 
 
 async def main():
     bluetooth_server = BluetoothConnection()
-    
+
     config = uvicorn.Config(app, host="0.0.0.0", port=5000)
     server = uvicorn.Server(config)
-    
+
     await asyncio.gather(
         bluetooth_server.start_server(),
         server.serve()
     )
+
 
 if __name__ == "__main__":
     try:
